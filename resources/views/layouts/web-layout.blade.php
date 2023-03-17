56c20b65c755eb40f3f1ace03a10cbecbfdd32ab
karimabdelaah <karim.abdullah@our-edu.net>
2023-03-17 02:46:44 +0200

    add site name

--- resources/views/layouts/web-layout.blade.php
+++ resources/views/layouts/web-layout.blade.php
@@ -3,11 +3,11 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Male_Fashion Template">
-    <meta name="keywords" content="Male_Fashion, unica, creative, html">
+    <meta name="description" content="{{env('APP_NAME')}}">
+    <meta name="keywords" content="{{env('APP_NAME')}}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Male-Fashion | Template</title>
+    <title>{{env('APP_NAME')}}</title>
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap"
